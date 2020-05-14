8a76d83e2cf1c63213fed6b078309d9f482c9e3d
tbalden <illespal@gmail.com>
2020-05-14 20:33:27 +0200

    cmdline: replace instead of remove for SafetyNet CTS pass
    Replace with "locked" fake values in fashion of Magisk,
    to make non magisked phones pass CTS safetynet check again.

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -21,8 +21,37 @@ static const struct file_operations cmdline_proc_fops = {
 	.release	= single_release,
 };
 
+static void replace_flag(char *cmd, const char *flag, const char *flag_new)
+{
+	char *start_addr, *end_addr;
+
+	// Ensure all instances of a flag are replaced
+	while ((start_addr = strstr(cmd, flag))) {
+		end_addr = strchr(start_addr, ' ');
+		if (end_addr)
+			memcpy(start_addr, flag_new, strlen(flag));
+		else
+			*(start_addr - 1) = '\0';
+	}
+}
+
+static void replace_safetynet_flags(char *cmd)
+{
+	// WARNING: be aware that you can't replace shorter string with longer ones in the function called here...
+	replace_flag(cmd, "androidboot.verifiedbootstate=orange",
+			  "androidboot.verifiedbootstate=green");
+}
+
 static int __init proc_cmdline_init(void)
 {
+	strcpy(new_command_line, saved_command_line);
+
+	/*
+	 * Replace various flags from command line seen by userspace in order to
+	 * pass SafetyNet CTS check.
+	 */
+	replace_safetynet_flags(new_command_line);
+
 	proc_create("cmdline", 0, NULL, &cmdline_proc_fops);
 	return 0;
 }
